fb5151ceb4000e31f4ccdf530dd1899007fa42dd
Immad <immad.naseer@gmail.com>
2020-04-19 17:37:31 -0700

    Solve Extreme Programming Challenge #14 from C2's wiki using Coyote

---
+++ ConcurrencyXP/CustomAsyncLock.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Coyote.Tasks;
+
+namespace ConcurrencyXP
+{
+    public class CustomAsyncLock
+    {
+        private AsyncLock asyncLock;
+        private AsyncLock.Releaser releaser;
+
+        public CustomAsyncLock()
+        {
+            this.asyncLock = AsyncLock.Create();
+        }
+        public async Task AcquireLock()
+        {
+            this.releaser = await this.asyncLock.AcquireAsync();
+        }
+
+        public void ReleaseLock()
+        {
+            this.releaser.Dispose();
+        }
+    }
+}
---
+++ ConcurrencyXP/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Coyote.Tasks;
+using System;
+using System.Collections.Generic;
+
+namespace ConcurrencyXP
+{
+    public class Program
+    {
+        public static BoundedBuffer boundedBuffer;
+
+        public static Task Reader(int taskIndex)
+        {
+            return Task.Run(async () =>
+            {
+                while (true)
+                {
+                    object x = await boundedBuffer.Take(taskIndex);
+                }
+            });
+        }
+
+        public static Task Writer(int taskIndex)
+        {
+            return Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await boundedBuffer.Put(taskIndex, new object());
+                }
+            });
+        }
+
+        [Microsoft.Coyote.SystematicTesting.Test]
+        public static async Task ReaderWriterTest()
+        {
+            Console.WriteLine("Starting ReaderWriterTest");
+
+            boundedBuffer = new BoundedBuffer(bufferLength: 1);
+
+            var tasks = new List<Task>();
+            tasks.Add(Reader(0));
+            tasks.Add(Reader(1));
+            tasks.Add(Writer(2));
+
+            await Task.WhenAll(tasks.ToArray());
+        }
+        
+        public static void Main(string[] args)
+        {
+            var task = ReaderWriterTest();
+            Task.WaitAll(task);
+        }
+    }
+}
